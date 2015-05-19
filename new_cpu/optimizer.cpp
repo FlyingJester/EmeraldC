4ad5b67a5b8a97f8023ca2d947d7bdea21dd4dcc
FlyingJester <foolkingcrown@gmail.com>
2015-05-19 11:14:41 -0800

    Added CPU template

---
+++ new_cpu/optimizer.cpp
@@ -0,0 +1,11 @@
+#include "optimizer.hpp"
+
+namespace Compiler{
+namespace new_cpu{
+
+int Optimize(Emitter *emit, unsigned level){
+    return -1;
+}
+
+}
+}
---
+++ new_cpu/optimizer.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "../asm.hpp"
+
+namespace Compiler{
+namespace new_cpu{
+
+int Optimize(Emitter *emit, unsigned level);
+
+}
+}
